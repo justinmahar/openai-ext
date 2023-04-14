8bc712cd05217223172bc9f3820cb18a716af63a
Justin Mahar <contact@justinmahar.com>
2023-04-14 16:19:16 -0400

    Build, version, and publish.

--- dist/stories/Example.stories.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-declare const _default: ComponentMeta<(args: any) => JSX.Element>;
-export default _default;
-export declare const Hello: ComponentStory<(args: any) => JSX.Element>;
-export declare const World: ComponentStory<(args: any) => JSX.Element>;
