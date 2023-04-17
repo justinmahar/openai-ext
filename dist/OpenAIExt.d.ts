7cbd3aa3eef8c2a576bf85d4a09e5d817cc69bac
Justin Mahar <contact@justinmahar.com>
2023-04-17 16:36:18 -0400

    Build, version, and publish.

--- dist/OpenAIExt.d.ts
+++ dist/OpenAIExt.d.ts
@@ -1,7 +1,35 @@
+export declare class OpenAIExt {
+    static V1_CHAT_COMPLETIONS_URL: string;
+    /**
+     * Stream the provided CreateChatCompletionRequest.
+     *
+     * Returns an XMLHttpRequest instance. To stop a completion, call `abort()` on the xhr instance.
+     *
+     * @param createChatCompletionRequest The completion request to stream. Pass the same argument you'd pass to the OpenAI API's `createChatCompletion` function.
+     * @param streamConfig The config for the request. This includes the API key and an optional endpoint URL. Uses v1 chat completions endpoint by default.
+     * @returns An XMLHttpRequest instance for the connection.
+     */
+    static streamClientChatCompletion(createChatCompletionRequest: any, streamConfig: ClientStreamChatCompletionConfig): XMLHttpRequest;
+    /**
+     * Stream the provided CreateChatCompletionRequest in Node.js or on the server. Config must include an `openai` instance that has been configured with the API key.
+     *
+     * To stop completion, call `stream.destroy()` or set up cancellation in the axios config. See https://axios-http.com/docs/cancellation.
+     *
+     * @param createChatCompletionRequest The completion request to stream. Pass the same argument you'd pass to the OpenAI API's `createChatCompletion` function.
+     * @param streamConfig The config for the stream request. This includes the configured `openai` instance.
+     * @param axiosConfig Optional axios config for the request.
+     */
+    static streamServerChatCompletion(createChatCompletionRequest: any, streamConfig: ServerStreamChatCompletionConfig, axiosConfig: any): void;
+    static parseContentDraft(dataString: string): ContentDraft;
+}
+export interface ContentDraft {
+    content: string;
+    isFinal: boolean;
+}
 /**
- * A handler containing callbacks that are called during the stream.
+ * A handler containing callbacks that are called during the stream on the client (browser).
  */
-export interface StreamChatCompletionHandler {
+export interface ClientStreamChatCompletionHandler {
     /**
      * Called when a content draft is received. The draft may be a partial completion. When the completion is done, `isFinal` will be true.
      *
@@ -26,9 +54,9 @@ export interface StreamChatCompletionHandler {
     onError?: (error: Error, status: number, xhr: XMLHttpRequest) => void;
 }
 /**
- * OpenAI configuration for the chat completion.
+ * OpenAI configuration for the client-side (browser) chat completion.
  */
-export interface StreamChatCompletionConfig {
+export interface ClientStreamChatCompletionConfig {
     /**
      * The API key to uses for the request.
      */
@@ -39,18 +67,47 @@ export interface StreamChatCompletionConfig {
      * Defaults to V1 chat completions, `OpenAIExt.V1_CHAT_COMPLETIONS_URL`.
      */
     chatCompletionsUrl?: string;
-}
-export declare class OpenAIExt {
-    static V1_CHAT_COMPLETIONS_URL: string;
     /**
-     * Stream the provided CreateChatCompletionRequest.
-     *
-     * Returns an XMLHttpRequest instance. To stop a completion, call `abort()` on the xhr instance.
-     *
-     * @param createChatCompletionRequest The completion request to stream. Pass the same argument you'd pass to the OpenAI API's `createChatCompletion` function.
-     * @param config The config for the request. This includes the API key and an optional endpoint URL. Uses v1 chat completions endpoint by default.
-     * @param handler A handler containing callbacks that are called during the stream.
-     * @returns An XMLHttpRequest instance for the connection.
+     * A handler containing callbacks that are called during the stream.
      */
-    static streamChatCompletion(createChatCompletionRequest: any, config: StreamChatCompletionConfig, handler?: StreamChatCompletionHandler): XMLHttpRequest;
+    handler?: ClientStreamChatCompletionHandler;
+}
+/**
+ * A handler containing callbacks that are called during the stream.
+ */
+export interface ServerStreamChatCompletionHandler {
+    /**
+     * Called when a content draft is received. The draft may be a partial completion. When the completion is done, `isFinal` will be true.
+     *
+     * @param contentDraft The content draft, which may only be a partial completion. When `isFinal` is true, the draft is complete and considered final.
+     * @param isFinal True when the completion finishes (the draft is final), false otherwise.
+     * @param stream The Axios request stream (of type IncomingMessage).
+     */
+    onContent?: (contentDraft: string, isFinal: boolean, stream: any) => void;
+    /**
+     * Called when the request is done.
+     *
+     * @param stream The Axios request stream (of type IncomingMessage).
+     */
+    onDone?: (stream: any) => void;
+    /**
+     * Called when there is an error during streaming. For example, 401 unauthorized when the API key is invalid.
+     *
+     * @param error The error reported.
+     * @param stream The Axios request stream (of type IncomingMessage).
+     */
+    onError?: (error: Error, stream: any) => void;
+}
+/**
+ * OpenAI configuration for the server-side (Node.js) chat completion.
+ */
+export interface ServerStreamChatCompletionConfig {
+    /**
+     * Configured OpenAI API instance (for server-side only).
+     */
+    openai: any;
+    /**
+     * A handler containing callbacks that are called during the stream.
+     */
+    handler?: ServerStreamChatCompletionHandler;
 }
--- dist/OpenAIExt.js
+++ dist/OpenAIExt.js
@@ -8,70 +8,123 @@ class OpenAIExt {
      * Returns an XMLHttpRequest instance. To stop a completion, call `abort()` on the xhr instance.
      *
      * @param createChatCompletionRequest The completion request to stream. Pass the same argument you'd pass to the OpenAI API's `createChatCompletion` function.
-     * @param config The config for the request. This includes the API key and an optional endpoint URL. Uses v1 chat completions endpoint by default.
-     * @param handler A handler containing callbacks that are called during the stream.
+     * @param streamConfig The config for the request. This includes the API key and an optional endpoint URL. Uses v1 chat completions endpoint by default.
      * @returns An XMLHttpRequest instance for the connection.
      */
-    static streamChatCompletion(createChatCompletionRequest, config, handler) {
+    static streamClientChatCompletion(createChatCompletionRequest, streamConfig) {
         var _a;
-        const apiKey = config.apiKey;
-        const url = (_a = config.chatCompletionsUrl) !== null && _a !== void 0 ? _a : OpenAIExt.V1_CHAT_COMPLETIONS_URL;
+        const apiKey = streamConfig.apiKey;
+        const url = (_a = streamConfig.chatCompletionsUrl) !== null && _a !== void 0 ? _a : OpenAIExt.V1_CHAT_COMPLETIONS_URL;
         const xhr = new XMLHttpRequest();
         xhr.open('POST', url);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.setRequestHeader('Authorization', 'Bearer ' + apiKey);
         xhr.onprogress = function (_event) {
-            if (handler && handler.onContent) {
-                const doneData = 'data: [DONE]';
-                const isFinal = xhr.responseText.includes(doneData);
-                const dataJsonLines = xhr.responseText
-                    .split('data: [DONE]')
-                    .join('')
-                    .trim()
-                    .split('data: ')
-                    .filter((v) => !!v);
-                const contentSnippets = dataJsonLines.map((dataJson) => {
-                    var _a, _b, _c;
-                    let parsed = undefined;
-                    try {
-                        parsed = JSON.parse(dataJson);
-                        return (_c = (_b = (_a = parsed === null || parsed === void 0 ? void 0 : parsed.choices[0]) === null || _a === void 0 ? void 0 : _a.delta) === null || _b === void 0 ? void 0 : _b.content) !== null && _c !== void 0 ? _c : '';
-                    }
-                    catch (e) {
-                        console.error(e);
-                    }
-                    return '';
-                });
-                const contentDraft = contentSnippets.join('');
-                handler.onContent(contentDraft, isFinal, xhr);
+            var _a;
+            if ((_a = streamConfig.handler) === null || _a === void 0 ? void 0 : _a.onContent) {
+                const dataString = xhr.responseText;
+                const contentDraft = OpenAIExt.parseContentDraft(dataString);
+                streamConfig.handler.onContent(contentDraft.content, contentDraft.isFinal, xhr);
             }
         };
         xhr.onreadystatechange = () => {
+            var _a, _b;
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 const status = xhr.status;
                 // In local files, status is 0 upon success in Mozilla Firefox
                 // See: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readystatechange_event#examples
                 if (status === 0 || (status >= 200 && status < 400)) {
                     // The request has been completed successfully
-                    if (handler && handler.onDone) {
-                        handler.onDone(xhr);
+                    if ((_a = streamConfig.handler) === null || _a === void 0 ? void 0 : _a.onDone) {
+                        streamConfig.handler.onDone(xhr);
                     }
                 }
                 else {
-                    if (handler && handler.onError) {
-                        handler.onError(new Error(`Error processing stream completion (XHR readyState ${xhr.readyState}, status ${xhr.status}).`), xhr.status, xhr);
+                    if ((_b = streamConfig.handler) === null || _b === void 0 ? void 0 : _b.onError) {
+                        streamConfig.handler.onError(new Error(`Error processing stream completion (XHR readyState ${xhr.readyState}, status ${xhr.status}).`), xhr.status, xhr);
                     }
                 }
             }
         };
         xhr.onerror = (event) => {
-            if (handler && handler.onError) {
-                handler.onError(new Error(`Error processing stream completion (XHR readyState ${xhr.readyState}, status ${xhr.status}).`), xhr.status, xhr);
+            var _a;
+            if ((_a = streamConfig.handler) === null || _a === void 0 ? void 0 : _a.onError) {
+                streamConfig.handler.onError(new Error(`Error processing stream completion (XHR readyState ${xhr.readyState}, status ${xhr.status}).`), xhr.status, xhr);
             }
         };
         const data = JSON.stringify(Object.assign(Object.assign({}, createChatCompletionRequest), { stream: true }));
         xhr.send(data);
         return xhr;
+    }
+    /**
+     * Stream the provided CreateChatCompletionRequest in Node.js or on the server. Config must include an `openai` instance that has been configured with the API key.
+     *
+     * To stop completion, call `stream.destroy()` or set up cancellation in the axios config. See https://axios-http.com/docs/cancellation.
+     *
+     * @param createChatCompletionRequest The completion request to stream. Pass the same argument you'd pass to the OpenAI API's `createChatCompletion` function.
+     * @param streamConfig The config for the stream request. This includes the configured `openai` instance.
+     * @param axiosConfig Optional axios config for the request.
+     */
+    static streamServerChatCompletion(createChatCompletionRequest, streamConfig, axiosConfig) {
+        const responsePromise = streamConfig.openai.createChatCompletion(Object.assign(Object.assign({}, createChatCompletionRequest), { stream: true }), Object.assign(Object.assign({}, axiosConfig), { responseType: 'stream' }));
+        responsePromise
+            .then((response) => {
+            const stream = response.data;
+            let dataString = '';
+            stream.on('data', (chunk) => {
+                var _a;
+                if ((_a = streamConfig.handler) === null || _a === void 0 ? void 0 : _a.onContent) {
+                    dataString += chunk.toString();
+                    const contentDraft = OpenAIExt.parseContentDraft(dataString);
+                    streamConfig.handler.onContent(contentDraft.content, contentDraft.isFinal, stream);
+                }
+            });
+            stream.on('end', () => {
+                var _a;
+                if ((_a = streamConfig.handler) === null || _a === void 0 ? void 0 : _a.onDone) {
+                    streamConfig.handler.onDone(stream);
+                }
+            });
+            stream.on('error', (e) => {
+                var _a;
+                if ((_a = streamConfig.handler) === null || _a === void 0 ? void 0 : _a.onError) {
+                    streamConfig.handler.onError(e, stream);
+                }
+            });
+        })
+            .catch((e) => {
+            var _a;
+            if ((_a = streamConfig.handler) === null || _a === void 0 ? void 0 : _a.onError) {
+                streamConfig.handler.onError(e, undefined);
+            }
+        });
+    }
+    static parseContentDraft(dataString) {
+        const doneData = 'data: [DONE]';
+        const isFinal = dataString.includes(doneData);
+        const dataJsonLines = dataString
+            .split('data: [DONE]')
+            .join('')
+            .trim()
+            .split('data: ')
+            .filter((v) => !!v);
+        const contentSnippets = dataJsonLines.map((dataJson) => {
+            var _a, _b, _c;
+            let parsed = undefined;
+            try {
+                parsed = JSON.parse(dataJson);
+                return (_c = (_b = (_a = parsed === null || parsed === void 0 ? void 0 : parsed.choices[0]) === null || _a === void 0 ? void 0 : _a.delta) === null || _b === void 0 ? void 0 : _b.content) !== null && _c !== void 0 ? _c : '';
+            }
+            catch (e) {
+                console.error(e);
+            }
+            return '';
+        });
+        const content = contentSnippets.join('');
+        return {
+            content,
+            isFinal,
+        };
     }
 }
 exports.OpenAIExt = OpenAIExt;
